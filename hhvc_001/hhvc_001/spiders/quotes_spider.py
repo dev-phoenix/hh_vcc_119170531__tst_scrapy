b8cbba9df490c20cb4bae0b3c803df1248eb807d
dev-phoenix <dev-phoenix@mail.ru>
2025-05-17 18:19:54 +0300

    building query with array of options

--- hhvc_001/hhvc_001/spiders/quotes_spider.py
+++ hhvc_001/hhvc_001/spiders/quotes_spider.py
@@ -6,6 +6,7 @@ tz: https://docs.google.com/document/d/\
 from pathlib import Path
 from copy import deepcopy as dp
 import json
+import urllib.parse as up
 
 import scrapy
 
@@ -93,12 +94,12 @@ https://alkoteka.com/web-api/v1/product
 """
 
 
-TESTURL = """
-https://alkoteka.com/web-api/v1/product
-?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416
-&options%5Bvid%5D[]=pivo-temnoe-filtrovannoe
-&options%5Bvid%5D[]=pivo-temnoe-nefiltrovannoe
-&page=1&per_page=20&root_category_slug=slaboalkogolnye-napitki-2
+TESTURL = """\
+https://alkoteka.com/web-api/v1/product\
+?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416\
+&options%5Bvid%5D[]=pivo-temnoe-filtrovannoe\
+&options%5Bvid%5D[]=pivo-temnoe-nefiltrovannoe\
+&page=1&per_page=20&root_category_slug=slaboalkogolnye-napitki-2\
 """
 
 """
@@ -118,6 +119,7 @@ class QuotesSpider(scrapy.Spider):
     cities_page = 1
     city_name = CITY_DEF
     city_uuid = CITY_UUID_DEF
+    product_urls = []
 
     async def start(self):
         """
@@ -135,8 +137,8 @@ class QuotesSpider(scrapy.Spider):
         #     # "https://quotes.toscrape.com/page/1/",
         #     # "https://quotes.toscrape.com/page/2/",
         # ]
-        url = CITIES_URL.format(page=self.cities_page)
-        yield scrapy.Request(url=url, callback=self.parse)
+        # url = CITIES_URL.format(page=self.cities_page)
+        # yield scrapy.Request(url=url, callback=self.parse)
         return self.get_cities()
 
     def get_cities(self):
@@ -160,14 +162,17 @@ class QuotesSpider(scrapy.Spider):
             url = CITIES_URL.format(page=self.cities_page)
             yield scrapy.Request(url=url, callback=self.parse_cities)
         else:
-            print("sites: ", self.cities)
+            # print("sites: ", self.cities)
             if CITY_SET in self.cities:
                 self.city_uuid = self.cities[CITY_SET]
                 self.city_name = CITY_SET
+            print("\033[1;30;1;47mselected location:", self.city_name, self.city_uuid, "\033[0m")
+
+            # collect category products
             urls = START_URLS
-            print("\033[1;30;1;47m", self.city_name, self.city_uuid, "\033[0m")
             for url in urls:
-                print(f"catalog url: {url}")
+                url = self.url_to_rest(url, page=1)
+                self.logp(f"catalog url: {url}")
                 # print(scrapy.Request(
                 # url=url, callback=self.parse_catalog).body)
                 yield scrapy.Request(url=url, callback=self.parse_catalog)
@@ -182,27 +187,135 @@ class QuotesSpider(scrapy.Spider):
             print(response)
         yield 'item'
 
+    def logp(self, *args, **kwargs):
+        """colored print
+        """
+        fg = kwargs.get('fg',30)
+        bg = kwargs.get('bg',47)
+        if 'fg' in kwargs:
+            del kwargs['fg']
+        if 'bg' in kwargs:
+            del kwargs['bg']
+        print()
+        print(f"\033[1;{fg};1;{bg}m", *args, "\033[0m", **kwargs)
+
+
+    def url_to_rest(self, url: str, **kwargs: dict)->str:
+        """convert public url to json request url
+        """
+        if 'web-api' in url:
+            return url
+
+        gate = 'https://alkoteka.com/web-api/v1/'
+        ptype = 'product'
+        cuuid = self.city_uuid
+        page = kwargs.get('page', 1)
+        per_page = kwargs.get('per_page', 100)
+        curl = url.replace('https://','')
+        curl = curl.replace('http://','')
+        curl = curl.split('/')
+
+        opts = []
+        opts.append(('city_uuid', cuuid))
+
+        if curl[1] == 'catalog':
+            catalog = curl[2]
+            _opts = curl[3:]
+            _opts = self.url_opts_parse(_opts)
+            opts.extend(_opts)
+            opts.append(('page', f'{page}'))
+            opts.append(('per_page', f'{per_page}'))
+            opts.append(('root_category_slug', f'{catalog}'))
+
+        self.logp('from url:', url, fg=31)
+        self.logp('curl:', curl)
+        self.logp('opts:', opts)
+
+        opts = up.urlencode(opts)
+        url = f'{gate}{ptype}?{opts}'
+
+        return url
+
+
+    def url_opts_parse(self, opts: list) -> list:
+        """Build options query dict
+        """
+        _opts = []
+        for opt in opts:
+            if opt.startswith('options'):
+                opt = opt.split('_')
+                key = f"options[{opt[0].split('-')[1]}][]"
+                # if key not in _opts:
+                #     _opts[key] = []
+                _opts.append((key, opt[1]))
+        return _opts
+
+
+    def url_page_increment(self, url:str) -> str:
+        """create url for next page
+        """
+        url = up.urlparse(url)
+        opts = up.parse_qsl(url.query)
+        self.logp('url_page_increment', opts, fg=34)
+        _opts = []
+        for opt in opts:
+            if opt[0] == 'page':
+                # opt[1] = int(opt[1]) + 1
+                tap = (opt[0], int(opt[1]) + 1)
+                _opts.append(tap)
+            else:
+                _opts.append(opt)
+        # page = int(opts['page'])
+        # opts['page'] = page + 1
+        opts = up.urlencode(_opts)
+        self.logp('urlencode', opts)
+        url = url._replace(query=opts)
+        url = up.urlunparse(url)
+        self.logp('url_page_increment url', url, fg=35)
+        return url
+
+
     def parse_catalog(self, response):
         """
         parse catalog pages
         """
-        print("status: {response.status}")
+        print(f"status: {response.status}")
         print("response: ", dir(response))
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
 
-        item = {
-            "title": response.css("title::text").get(),
-            "description": response.css(
-                'meta[name="description"]::attr(content)'
-            ).get(),
-        }
-        item = dp(OUT_TPL)
-        item["url"] = response.url
-        item["title"] = response.css("title::text").get()
-        yield item
+        url = response.url
+        self.logp("url:", url, fg=31, bg=46)
+
+        data = response.json()
+        # print("data: ", data)
+
+        self.product_urls.extend(res['product_url'] for res in data['results'])
+        self.log("\033[1;30;1;47mproducts count:"\
+                + str(len(self.product_urls)) + "\033[0m")
+
+        if data['meta']['has_more_pages'] :
+            url = self.url_page_increment(url)
+            self.logp(f"parse catalog url: {url}")
+            self.log("\033[1;30;1;47mcatalog url:" + url + "\033[0m")
+            # print(scrapy.Request(
+            # url=url, callback=self.parse_catalog).body)
+            yield scrapy.Request(url=url, callback=self.parse_catalog)
+
+        # page = response.url.split("/")[-2]
+
+        # filename = f"quotes-{page}.html"
+        # Path(filename).write_bytes(response.body)
+        # self.log(f"Saved file {filename}")
+
+        # item = {
+        #     "title": response.css("title::text").get(),
+        #     "description": response.css(
+        #         'meta[name="description"]::attr(content)'
+        #     ).get(),
+        # }
+        # item = dp(OUT_TPL)
+        # item["url"] = response.url
+        # item["title"] = response.css("title::text").get()
+        # yield item
 
         # next_page = response.css("li.next a::attr(href)").get()
         # urls = response.css("li.next a::attr(href)").get()
